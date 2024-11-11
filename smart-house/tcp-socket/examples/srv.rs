d52152aa23e5e1245244f70ab6eae6405f89837b
Andrey Khalepov <w54661c@gmail.com>
2024-11-11 22:06:53 +1000

    tcp_socket done

--- smart-house/tcp-socket/examples/srv.rs
+++ smart-house/tcp-socket/examples/srv.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
     let mut server = tcp_socket::SocketServer::new("127.0.0.1:3456")?;
     server.serve()?;
     Ok(())
--- smart-house/tcp-socket/src/server.rs
+++ smart-house/tcp-socket/src/server.rs
@@ -1,6 +1,7 @@
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream, ToSocketAddrs};
+use std::net::{TcpListener, ToSocketAddrs};
 use std::str;
+use tracing::{error, info};
 
 use crate::error::Result;
 
@@ -24,44 +25,76 @@ impl SocketServer {
     }
 
     pub fn serve(&mut self) -> Result<()> {
-        println!("Listening...");
+        info!("Listening...");
         loop {
             let (mut stream, _) = self.listener.accept()?;
             let mut buf = vec![0; 4];
             stream.read_exact(&mut buf)?;
             if let Ok(cmd) = str::from_utf8(&buf) {
-                self.process_data(cmd, stream)?
+                let res = self.process_command(cmd);
+                stream.write_all(&res)?
             } else {
-                eprintln!("Bad data received")
+                error!("Bad data received")
             }
         }
     }
 
-    fn process_data(&mut self, data: &str, mut stream: TcpStream) -> Result<()> {
-        match data {
+    fn process_command(&mut self, cmd: &str) -> Vec<u8> {
+        match cmd {
             "cmd1" => {
-                println!("Status requested");
+                info!("Status requested");
                 let prefix = b"state:".as_slice();
                 let pwr_bytes = self.power.to_be_bytes();
                 let pwr_bytes_slice = pwr_bytes.as_slice();
                 let state_array = &[self.is_turned_on as u8];
-                let response = [prefix, state_array, pwr_bytes_slice].concat();
-                stream.write_all(&response)?;
+                [prefix, state_array, pwr_bytes_slice].concat()
             }
             "cmd2" => {
-                println!("Turn on requested");
+                info!("Turn on requested");
                 self.is_turned_on = true;
-                self.power = 12.5; // Здесь можно было использовать random, но все равно это будут моковые данные...
-                stream.write_all(b"done")?;
+                self.power = 12.5;
+                b"done".into()
             }
             "cmd3" => {
-                println!("Turn off requested");
+                info!("Turn off requested");
                 self.is_turned_on = false;
                 self.power = 0.0;
-                stream.write_all(b"done")?;
+                b"done".into()
             }
-            _ => eprintln!("Unknown command received"),
+            _ => vec![],
         }
-        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_process_command1() {
+        let mut srv = SocketServer::new("127.0.0.1:5000").unwrap();
+        let _ = srv.process_command("cmd2");
+        let res = srv.process_command("cmd1");
+        assert_eq!(*b"state:", res[..6]);
+        assert_eq!(1, res[6]);
+        assert_eq!(12.5_f64.to_be_bytes(), res[7..]);
+    }
+
+    #[test]
+    fn test_process_command2() {
+        let mut srv = SocketServer::new("127.0.0.1:5001").unwrap();
+        let res = srv.process_command("cmd2");
+        assert_eq!(*b"done", *res);
+        assert_eq!(true, srv.is_turned_on);
+        assert_eq!(12.5, srv.power);
+    }
+
+    #[test]
+    fn test_process_command3() {
+        let mut srv = SocketServer::new("127.0.0.1:5002").unwrap();
+        let res = srv.process_command("cmd3");
+        assert_eq!(*b"done", *res);
+        assert_eq!(false, srv.is_turned_on);
+        assert_eq!(0.0, srv.power);
     }
 }
